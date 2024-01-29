c2ecd8ca36a9d5610eedd6ffaffdbf274d05c3e9
Baba <jagasal@icloud.com>
2024-01-28 20:33:35 -0600

    deleted unnecessary images, new js

--- active-link.js
+++ active-link.js
@@ -1,8 +1,18 @@
-document.querySelectorAll(".nav-link").forEach((ele) =>
-  ele.addEventListener("click", function (event) {
-    document
-      .querySelectorAll(".nav-link")
-      .forEach((ele) => ele.classList.remove("active"));
-    this.classList.add("active");
-  })
-);
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("nav a");
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
